aca8e7b888aa5a58a0fbab84e71932baf6fce353
FarrosArya <141887224+FarrosArya@users.noreply.github.com>
2024-01-10 21:27:16 +0700

    Add files via upload

---
+++ testBusPratikum.java
@@ -0,0 +1,13 @@
+public class testBusPratikum {
+    public static void main(String[] args) {
+        BusPratikum busMini=new BusPratikum(10);
+        busMini.addPenumpang(5);
+        busMini.addPenumpang(6);
+        busMini.getPenumpang(10);
+        busMini.getPenumpang(15);
+        busMini.cetak();
+
+        busMini.getAverage();
+        busMini.cetak();
+    }
+}
